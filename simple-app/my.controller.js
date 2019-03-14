f27f25c7f2c76e120aa4b78803f160897daf252b
Sravan Kalluri <sravan.kalluri@gmail.com>
2019-03-14 01:08:33 -0700

    Updated to latest enton

--- simple-app/my.controller.js
+++ simple-app/my.controller.js
@@ -1,10 +1,18 @@
 import { Controller, Get } from "enton";
+import { Post } from "enton/lib/decorators";
 
 @Controller("/my")
 class MyController {
   @Get("/:name")
-  index(req, res) {
-    res.send(`Hey there ${req.params.name}`);
+  async index(name, { query }) {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(`Hey there ${name} ${query.age}`), 300);
+    });
+  }
+
+  @Post()
+  async update(body) {
+    return Promise.resolve(body);
   }
 }
 
